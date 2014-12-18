d6674b06ea057c1490804c0820907620c7d6517d
Keshav Santhanam <santhnm2@illinois.edu>
2014-12-18 11:36:31 -0600

    added search bar

--- js/Itunes.js
+++ js/Itunes.js
@@ -1,5 +1,29 @@
-/**
- * @jsx React.DOM
- */
+var BASE = 'https://itunes.apple.com/search/';
 
- module.exports = Itunes;
+function getURL(input) {
+	var url = BASE;
+	var searchTerm = input.replace(" ", "%20");
+	var parameters = {
+		"term": searchTerm,
+		"country": "us",
+		"entity": "software",
+		"limit": 5
+	}
+	var first = true;
+	for (var parameter in parameters) {
+		if (parameters.hasOwnProperty(parameter)) {
+			if (first) {
+				url += '?';
+				first = false;
+			}
+			else {
+				url += '&';
+			}
+			url += parameter + '=' + parameters[parameter];
+		}
+	}
+	return url;
+}
+
+
+module.exports = {getURL: getURL};
--- js/Search.react.js
+++ js/Search.react.js
@@ -8,17 +8,39 @@ var Navbar = require('react-bootstrap/Navbar');
 var DropdownButton = require('react-bootstrap/DropdownButton');
 var NavItem = require('react-bootstrap/NavItem');
 var MenuItem = require('react-bootstrap/MenuItem');
-//var Itunes = require('Itunes');
+var Input = require('react-bootstrap/Input');
+var Button = require('react-bootstrap/Button');
+var Itunes = require('./Itunes');
 
 var Search = React.createClass({
 	getInitialState: function() {
-		return null;
+		return {
+			"searchTerm": null
+		};
 	},
+	
+	_handleSubmit: function() {
+		var URL = Itunes.getURL(this.state.searchTerm));
+		
+        //console.log(this.refs.searchBox.getDOMNode().value);
+        //this.setState({text: ''});
+    },
+
+    _handleChange: function(evt) {
+        this.setState({searchTerm: evt.target.value});
+    },
+
+	_handleKeyDown: function(evt) {
+        if (evt.keyCode == 13 ) {
+            return this._handleSubmit();
+        }
+    },
 
 	render: function() {
+		var searchStyle = {"marginTop":"10%"};
 		return(
 			<div>
-				<Navbar fixedTop="true" fluid="true" brand="Appshare" role="navigation">
+				<Navbar fixedTop fluid brand="Appshare" role="navigation">
 					<Nav>
 						<NavItem eventKey={1} href="friends.html">My Friends</NavItem>
         				<NavItem eventKey={2} href="favorites.html">My Favorites</NavItem>
@@ -28,6 +50,9 @@ var Search = React.createClass({
         				</DropdownButton>
 					</Nav>
 				</Navbar>
+				<form style={searchStyle}>
+					<Input ref="searchBox" type="text" placeholder="Search for an app..." onChange={this._handleChange} onKeyDown={this._handleKeyDown}/>
+				</form>
 			</div>
 		);
 	}
